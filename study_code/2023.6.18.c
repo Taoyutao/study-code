cffdb0c3907508a31e93af64a4f333f207f82e84
Taoyutao <3183715114@qq.com>
2023-06-18 17:40:48 +0800

    提交代码

--- study_code/2023.6.18.c
+++ study_code/2023.6.18.c
@@ -1,16 +1,38 @@
+//两个数值比较大小
+// 
+//#include<stdio.h>
+//int Max(int x,int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//int main()
+//{
+//	int a, b, max;
+//	scanf_s("%d %d",&a,&b);
+//	max = Max(a,b);
+//	printf("max = %d\n",max);
+//	return 0;
+//}
+
+
+//三个数值比较大小
+
 #include<stdio.h>
-int Max(int x,int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
-}
 int main()
 {
-	int a, b, max;
-	scanf_s("%d %d",&a,&b);
-	max = Max(a,b);
+	int a, b, c,max;
+	scanf_s("%d %d %d",&a,&b,&c);
+	if (a > b)
+		max = a;
+	else
+		max = b;
+	if (c > max)
+		max = c;
+
 	printf("max = %d\n",max);
+
 	return 0;
 }
