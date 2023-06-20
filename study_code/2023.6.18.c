86bbabde0a99fc3ce2f8623ff40da5185898033d
Taoyutao <3183715114@qq.com>
2023-06-20 12:23:27 +0800

    提交代码

--- study_code/2023.6.18.c
+++ study_code/2023.6.18.c
@@ -20,19 +20,34 @@
 
 //三个数值比较大小
 
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c,max;
+//	scanf_s("%d %d %d",&a,&b,&c);
+//	if (a > b)
+//		max = a;
+//	else
+//		max = b;
+//	if (c > max)
+//		max = c;
+//
+//	printf("max = %d\n",max);
+//
+//	return 0;
+//}
+
+
+//写出1-100之间的奇数
 #include<stdio.h>
 int main()
 {
-	int a, b, c,max;
-	scanf_s("%d %d %d",&a,&b,&c);
-	if (a > b)
-		max = a;
-	else
-		max = b;
-	if (c > max)
-		max = c;
-
-	printf("max = %d\n",max);
-
+	int i = 1;
+	while (i <= 100)
+	{
+		if (i % 2 != 0)
+			printf("%d ",i);
+		i++;
+	}
 	return 0;
 }
